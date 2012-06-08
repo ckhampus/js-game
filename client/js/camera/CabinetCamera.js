abbab2676fbda20c2e224665e94b872500d11555
Cristian Hampus <contact@cristianhampus.se>
2012-06-08 08:04:53 +0200

    First basic implementation of a cabinet camera.

--- client/js/camera/CabinetCamera.js
+++ client/js/camera/CabinetCamera.js
@@ -1,16 +1,18 @@
 define(function (require) {
   var THREE = require('three');
 
-  var CabinetCamera = function (left, right, top, bottom, near, far) {
-    THREE.OrthographicCamera.call( this );
+  var CabinetCamera = function (left, right, top, bottom, near, far, angle) {
+    THREE.OrthographicCamera.call(this);
 
     this.left = left;
     this.right = right;
     this.top = top;
     this.bottom = bottom;
 
-    this.near = ( near !== undefined ) ? near : 0.1;
-    this.far = ( far !== undefined ) ? far : 2000;
+    this.near = (near !== undefined) ? near : 0.1;
+    this.far = (far !== undefined) ? far : 2000;
+
+    this.angle = (angle !== undefined) ? angle : 0;
 
     this.updateProjectionMatrix();
   };
@@ -18,32 +20,18 @@ define(function (require) {
   CabinetCamera.prototype = new THREE.OrthographicCamera();
   CabinetCamera.prototype.constructor = CabinetCamera;
 
-  /*
-  THREE.OrthographicCamera = function ( left, right, top, bottom, near, far ) {
+  CabinetCamera.prototype.updateProjectionMatrix = function () {
+    this.projectionMatrix.makeOrthographic(this.left, this.right, this.top, this.bottom, this.near, this.far);
 
-    THREE.Camera.call( this );
+    var cabinet = new THREE.Matrix4(
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      (-0.5 * Math.cos(this.angle)), (-0.5 * Math.sin(this.angle)), 1, 0,
+      0, 0, 0, 1
+    );
 
-    this.left = left;
-    this.right = right;
-    this.top = top;
-    this.bottom = bottom;
-
-    this.near = ( near !== undefined ) ? near : 0.1;
-    this.far = ( far !== undefined ) ? far : 2000;
-
-    this.updateProjectionMatrix();
-
+    this.projectionMatrix.multiplySelf(cabinet);
   };
 
-  THREE.OrthographicCamera.prototype = new THREE.Camera();
-  THREE.OrthographicCamera.prototype.constructor = THREE.OrthographicCamera;
-
-  THREE.OrthographicCamera.prototype.updateProjectionMatrix = function () {
-
-    this.projectionMatrix.makeOrthographic( this.left, this.right, this.top, this.bottom, this.near, this.far );
-
-  };
-  */
- 
- return CabinetCamera;
+  return CabinetCamera;
 });
